6f37dd8058de64e4b9c469ceb2b985ba2c61a008
Winston Tsia <winstontsia@gmail.com>
2019-10-21 12:17:10 -0700

    touched up the seeds file and the model

--- models/questionModel.js
+++ models/questionModel.js
@@ -1,9 +1,11 @@
 const mongoose = require("../db/connection");
 
 const questionSchema = new mongoose.Schema({
-  id: Number,
   question: String,
-  answer: String
+  hint: String,
+  answer: String,
+  url: String,
+  type: String
 });
 
 const questionModel = mongoose.model("question", questionSchema);
