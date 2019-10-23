b7113d85de487383476eb4d924c079f2089a3d42
Gabriel Sharpton <3bag@Tricias-MBP.lan1>
2019-10-23 11:59:15 -0500

    override

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const myController = require('./controllers/myItems');
 const parser = require('body-parser');
 const cors = require("cors");
+const methodOverride = require("method-override");
 
 //initiate express
 const app = express();
@@ -9,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
-
+app.use(methodOverride(_method));
 app.use("/", myController);
 
 app.set("port", process.env.PORT || 3000);
